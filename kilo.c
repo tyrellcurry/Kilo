97d46071271aef97d551c57573d5aa43fceaf92b
tyrellcurry <tyrellcurryio@gmail.com>
2025-05-30 21:03:36 -0700

    Initialized

---
+++ kilo.c
@@ -0,0 +1,68 @@
+// https://viewsourcecode.org/snaptoken/kilo
+/*** includes ***/
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** data ***/
+struct termios orig_termios;
+
+/*** terminal ***/
+
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
+void raw_mode_disable(void) {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void raw_mode_enable(void) {
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    die("tcgetattr");
+
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(raw_mode_disable);
+  struct termios raw = orig_termios;
+
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+    die("tcsetattr");
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+/*** init ***/
+
+int main(void) {
+  raw_mode_enable();
+
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+      die("read");
+    read(STDIN_FILENO, &c, 1);
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q')
+      break;
+  }
+
+  return 0;
+}
